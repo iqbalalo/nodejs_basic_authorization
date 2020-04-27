c8643eeec0255c72f44398c4e4c4ac438e002aab
Iqbal Hossain <iqbal@kotozna.com>
2020-04-27 14:16:01 +0900

    first commit

--- .history/app_20200427113625.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3000;
-
-const server = app.listen(port, () => {
-  console.log("Server is listening on", server.address().port);
-});
--- .history/authorization_20200427115210.js
+++
@@ -1,9 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-module.exports = () => {
-  return (req, res, next) => {
-    console.log("Authorization Middleware");
-
-    next();
-  };
-};
